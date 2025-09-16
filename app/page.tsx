f41e73d563f85227aaeafc451a669d09851857b3
Ed Dowding <me@eddowding.com>
2025-09-16 18:11:30 +0100

    Trigger Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ import {
 } from 'lucide-react'
 
 export default function HomePage() {
+  // DocPulse-style homepage
   return (
     <div className="min-h-screen bg-white">
       {/* Navigation */}
